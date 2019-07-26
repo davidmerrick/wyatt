1cd1c73031f9c2ddc424d331433ec1e58cf5ab7c
David Merrick <david@david-merrick.com>
2019-07-26 15:13:18 -0700

    Testing a write to the s3 bucket

--- src/main/kotlin/com/merricklabs/wyatt/handlers/logic/WyattLogic.kt
+++ src/main/kotlin/com/merricklabs/wyatt/handlers/logic/WyattLogic.kt
@@ -1,12 +1,19 @@
 package com.merricklabs.wyatt.handlers.logic
 
+import com.merricklabs.wyatt.config.WyattConfig
+import com.merricklabs.wyatt.external.aws.WyattS3Client
 import mu.KotlinLogging
 import org.koin.core.KoinComponent
+import org.koin.core.inject
 
 private val log = KotlinLogging.logger {}
 
 class WyattLogic : KoinComponent {
+    private val config by inject<WyattConfig>()
+    private val wyattS3Client by inject<WyattS3Client>()
+
     fun handleRequest() {
         log.info("Received event")
+        wyattS3Client.s3.putObject(config.s3BucketName, "foo", "banana")
     }
 }
