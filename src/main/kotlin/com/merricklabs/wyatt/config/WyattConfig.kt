026a3906484b683c2e17527431c1ee59db21b4cb
David Merrick <david@david-merrick.com>
2019-07-27 23:31:33 -0700

    Rudimentary fetch of bill

--- src/main/kotlin/com/merricklabs/wyatt/config/WyattConfig.kt
+++ src/main/kotlin/com/merricklabs/wyatt/config/WyattConfig.kt
@@ -8,4 +8,10 @@ class WyattConfig : KoinComponent {
     val s3BucketName: String = System.getenv("S3_BUCKET_NAME")
     val s3Endpoint: String? = System.getenv("S3_ENDPOINT")
     val awsRegion: String = System.getenv("AWS_REGION") ?: DEFAULT_REGION
+    val verizon = Verizon()
+
+    class Verizon {
+        val username: String = System.getenv("VERIZON_USERNAME")
+        val password: String = System.getenv("VERIZON_PASSWORD")
+    }
 }
--- src/main/kotlin/com/merricklabs/wyatt/handlers/logic/WyattLogic.kt
+++ src/main/kotlin/com/merricklabs/wyatt/handlers/logic/WyattLogic.kt
@@ -1,8 +1,11 @@
 package com.merricklabs.wyatt.handlers.logic
 
 import com.codeborne.selenide.webdriver.WebDriverFactory
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.merricklabs.wyatt.config.WyattConfig
 import com.merricklabs.wyatt.external.aws.WyattS3Client
+import com.merricklabs.wyatt.external.verizon.VerizonClient
+import com.merricklabs.wyatt.pages.LoginPage
 import mu.KotlinLogging
 import org.koin.core.KoinComponent
 import org.koin.core.inject
@@ -14,17 +17,29 @@ class WyattLogic : KoinComponent {
     private val config by inject<WyattConfig>()
     private val wyattS3Client by inject<WyattS3Client>()
     private val driverFactory by inject<WebDriverFactory>()
+    private val loginPage by inject<LoginPage>()
+    private val verizonClient by inject<VerizonClient>()
+    private val mapper by inject<ObjectMapper>()
+
     private val driver: WebDriver
 
     init {
         driver = driverFactory.createWebDriver(null, null)
     }
 
-
     fun handleRequest() {
         log.info("Spinning up Chrome")
-        driver.get("https://www.google.com")
-        log.info("url: ${driver.currentUrl}")
-//        wyattS3Client.s3.putObject(config.s3BucketName, "foo", "banana")
+        loginPage.goto()
+        loginPage.enterUsername(config.verizon.username)
+        loginPage.enterPassword(config.verizon.password)
+        loginPage.submit()
+        Thread.sleep(2000)
+
+        val bill = verizonClient.fetchBill(driver.manage().cookies)
+        wyattS3Client.s3.putObject(
+                config.s3BucketName,
+                "bill",
+                mapper.writeValueAsString(bill)
+        )
     }
 }
